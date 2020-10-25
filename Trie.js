2bd12a973f825c09caa76ecc9ea7488bb1ae49c2
Igor <houwat@gmail.com>
2020-10-25 13:25:59 -0400

    trie -findWord and getWords methods

--- Trie.js
+++ Trie.js
@@ -22,17 +22,28 @@ class Trie {
         if(word.length === 0) return null;
         let current = this.characters;
         while(index < word.length - 1){
-                if(Object.keys(current).length){
-                    current = current[word[index]].characters
-                    index++;
-                }
+            if(Object.keys(current).length === 0 || !current[word[index]]) return undefined;
+            current = current[word[index]].characters;
+            index++;
             }
-        current[word[index]].isWord ? true : undefined
+        if(current[word[index]] && current[word[index]].isWord){
+            return current[word[index]];
+        } else return undefined;
+    }
+    getWords(words = [], currentWord = ""){
+        if(this.isWord) words.push(currentWord);
+        for (let letter in this.characters){
+            let nextWord = currentWord + letter;
+            this.characters[letter].getWords(words, nextWord);
+        }
+        return words;
     }
 }
 
 let trie = new Trie();
 trie.addWord('do');
 trie.addWord('dot');
+trie.addWord('dope');
 trie.addWord('oops');
-trie.findWord('dotys');
+// trie.findWord('doe'); // undefined
+trie.getWords();
